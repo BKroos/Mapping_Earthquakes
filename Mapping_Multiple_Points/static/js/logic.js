fad6550f92a8816ce7f71c05df621a42c50a3d09
BKroos <btkroos@gmail.com>
2021-07-15 18:36:22 -0500

    Added additional city data and markers

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,25 @@
+console.log('working');
+
+let map=L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.Circlemarker(city.location, {
+        radius:city.population/10000
+    }
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
